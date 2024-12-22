d26ec7ba92237a4e50c77bcc4d783c0319819b39
PavelBeloded <beloded_2007@bk.ru>
2024-12-23 00:48:59 +0300

    random bg

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,48 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StatusBar, Text, TouchableOpacity, View, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function Index(): JSX.Element {
+
+  const [randomBackground, setRandomBackground] = useState("#fff")
+
+  const generateColor = () =>{
+    const hexRange = '0123456789ABCDEF'
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)]
+    }
+    setRandomBackground(color)
+  }
+
   return (
-    <View
-    
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={randomBackground}/>
+      <View style={[styles.container, {backgroundColor: randomBackground}]}>
+      <TouchableOpacity onPress={generateColor}>
+        <View style={styles.actionbBtn}>
+          <Text style={styles.actionbBtnTxt}>Press me</Text>
+        </View>
+      </TouchableOpacity>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionbBtn : {
+    borderRadius: 12,
+    backgroundColor: "#6A1B4D",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionbBtnTxt:{
+    fontSize: 24,
+    color: "#fff",
+    textTransform: 'uppercase',
+  }
+})
